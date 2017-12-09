343beceabc9175d39087eeb6ca5b2dc0be6bde79
hippolyte-dubois <hippolyte.dubois@etu.unilim.fr>
2017-12-09 22:46:17 +0100

    init

---
+++ script.js
@@ -0,0 +1,19 @@
+var arr = "$ATAN&MEME$".split('');
+var n = 100;
+var t = 0;
+var rate = 500;
+function display(){
+    var bandeaux = document.getElementsByClassName("bandeau");
+    var offset = 0;
+    Array.prototype.forEach.call(bandeaux, function(el) {
+        var s = "";
+        for(i = t + offset ; i - t - offset < n ; i++){
+            s = s.concat(arr[i % arr.length]);
+        }
+        el.innerHTML = s;
+        offset += 5;
+    }); 
+    t++;
+    var c = setTimeout(display, rate);
+
+}
